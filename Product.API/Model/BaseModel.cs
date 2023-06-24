c12f3b550056d519e0db4aa8fb81948ad353e8c6
Muhammad Ihsan <mihsan191@gmail.com>
2023-06-24 14:24:59 +1000

    feat: Add product entity/domain

---
+++ Product.API/Model/BaseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Model
+{
+	public class BaseModel
+    {
+        public Guid Id { get; set; }
+
+        // We can also add other fields like CreatedDate, UpdatedDate here
+    }
+}
+
---
+++ Product.API/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Model
+{
+	public class Product : BaseModel
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int Price { get; set; }
+	}
+}
+
